146f56e4283e1f28f1db8dcf78f9c1680cd71fad
Luciggl <lucielionascimento19@gmail.com>
2026-08-13 21:20:54 -0300

    feat: create ProductEntity

---
+++ src/main/java/com/luciggl/dev/SistemaRestaurante/entities/Product.java
@@ -0,0 +1,92 @@
+package com.luciggl.dev.SistemaRestaurante.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_product", unique = true)
+    private Long id;
+    @Column(name = "name_product", unique = true, nullable = false)
+    private String name;
+    @Column(name = "description_product", nullable = false)
+    private String description;
+    @Column(name = "price_product", nullable = false)
+    private BigDecimal price;
+    @Column(name = "img_product", unique = true, nullable = false)
+    private String image;
+    @Column(name = "available_product", nullable = false)
+    private Boolean available;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, BigDecimal price, String image, Boolean available) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+        this.available = available;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
