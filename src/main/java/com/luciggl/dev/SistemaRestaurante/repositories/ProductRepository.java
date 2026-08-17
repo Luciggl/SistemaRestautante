24c8ee0fd488e7fc25271cdb3adf40ec712f8211
Luciggl <lucielionascimento19@gmail.com>
2026-08-17 20:24:02 -0300

    feat: integration JpaRepository interface

--- src/main/java/com/luciggl/dev/SistemaRestaurante/repositories/ProductRepository.java
+++ src/main/java/com/luciggl/dev/SistemaRestaurante/repositories/ProductRepository.java
@@ -3,5 +3,5 @@ package com.luciggl.dev.SistemaRestaurante.repositories;
 import com.luciggl.dev.SistemaRestaurante.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface productRepository extends JpaRepository<ProductEntity, Long> {
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 }
