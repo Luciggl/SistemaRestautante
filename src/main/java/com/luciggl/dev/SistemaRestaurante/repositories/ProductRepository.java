5f04ba8c4539e17da5a91b43d58e1073d589f643
Luciggl <lucielionascimento19@gmail.com>
2026-08-18 21:25:00 -0300

    feat: Creating the search by category

--- src/main/java/com/luciggl/dev/SistemaRestaurante/repositories/ProductRepository.java
+++ src/main/java/com/luciggl/dev/SistemaRestaurante/repositories/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.luciggl.dev.SistemaRestaurante.repositories;
 
 import com.luciggl.dev.SistemaRestaurante.entities.ProductEntity;
+import com.luciggl.dev.SistemaRestaurante.enums.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+    List<ProductEntity> findByCategory(ProductCategory category);
 }
--- src/main/java/com/luciggl/dev/SistemaRestaurante/services/ProductService.java
+++ src/main/java/com/luciggl/dev/SistemaRestaurante/services/ProductService.java
@@ -1,8 +1,10 @@
 package com.luciggl.dev.SistemaRestaurante.services;
 
 import com.luciggl.dev.SistemaRestaurante.entities.ProductEntity;
+import com.luciggl.dev.SistemaRestaurante.enums.ProductCategory;
 import com.luciggl.dev.SistemaRestaurante.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,5 +17,18 @@ public class ProductService {
 
     public Optional<List<ProductEntity>> getProducts(){
         return Optional.of(repository.findAll());
+    }
+
+    public HttpStatus createProduct(ProductEntity product){
+        repository.save(product);
+        return HttpStatus.ACCEPTED;
+    }
+
+    public Optional<ProductEntity> getProductById(Long id){
+            return repository.findById(id);
+    }
+
+    public List<ProductEntity> getProductByCategory(ProductCategory category){
+        return repository.findByCategory(category);
     }
 }
